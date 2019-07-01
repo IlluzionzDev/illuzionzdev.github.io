e73da33c5d70556b25cfaef408da7e0e2b8f5ca0
Illuzionz <jaminstratford@gmail.com>
2019-07-02 07:57:26 +1200

    Add button functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,12 @@
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
